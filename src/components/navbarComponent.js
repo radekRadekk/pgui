e54bd6f31e4ca5c47ae984d7229f7822c53bc052
Radek C <=>
2021-12-07 23:41:37 +0100

    Update navbar

--- src/components/navbarComponent.js
+++ src/components/navbarComponent.js
@@ -15,15 +15,11 @@ export default function NavbarComponent() {
 
   return (
     <Box sx={{ flexGrow: 1 }} >
-      <AppBar position="static" >
+      <AppBar position="static" style={{
+        borderRadius: "20px",
+        background: "gray"
+      }}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          ></IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             PGUI
           </Typography>
