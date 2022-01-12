cdea2140606b5eb296296fcd6b850539a72570c7
Radek C <=>
2022-01-12 19:00:04 +0100

    Add fetching data for ordersComponent

--- src/components/ordersComponent.js
+++ src/components/ordersComponent.js
@@ -1,38 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Typography from "@material-ui/core/Typography";
 import { useTranslation } from "react-i18next";
 import { Grid } from "@mui/material";
 import OrderCategoryComponent from "./s/orderCategoryComponent"
 
 export default function OrdersComponent() {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
+  const [data, setData] = useState({
+    "unpaid": 0,
+    "unsent": 0,
+    "returns": 0
+  })
 
-    return <Grid container
-        spacing={1}
-        borderRadius="20px"
-        bgcolor="#ffffff"
-        margin="auto">
-        <Grid item xs={12}>
-            <Grid container justifyContent="center">
-                <Grid item>
-                    <Typography variant="h5">
-                        {t("orders")}
-                    </Typography>
-                </Grid>
-            </Grid>
+  const nickname = "JanKowalski";
+
+  useEffect(() => {
+    loadData();
+  }, [data])
+
+  const loadData = () => {
+    fetch(`http://127.0.0.1:5000/users/${nickname}/orders`)
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      });
+  };
+
+  return <Grid container
+    spacing={1}
+    borderRadius="20px"
+    bgcolor="#ffffff"
+    margin="auto">
+    <Grid item xs={12}>
+      <Grid container justifyContent="center">
+        <Grid item>
+          <Typography variant="h5">
+            {t("orders")}
+          </Typography>
         </Grid>
-        <Grid item xs={12} padding="10px">
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={4} padding="10px">
-                    <OrderCategoryComponent name={t("unpaid")} number="12" />
-                </Grid>
-                <Grid item xs={12} md={4} padding="10px">
-                    <OrderCategoryComponent name={t("unsent")} number="3" />
-                </Grid>
-                <Grid item xs={12} md={4} padding="10px">
-                    <OrderCategoryComponent name={t("returns")} number="5" />
-                </Grid>
-            </Grid>
+      </Grid>
+    </Grid>
+    <Grid item xs={12} padding="10px">
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={4} padding="10px">
+          <OrderCategoryComponent name={t("unpaid")} number={data['unpaid']} />
         </Grid>
-    </Grid>;
+        <Grid item xs={12} md={4} padding="10px">
+          <OrderCategoryComponent name={t("unsent")} number={data['unsent']} />
+        </Grid>
+        <Grid item xs={12} md={4} padding="10px">
+          <OrderCategoryComponent name={t("returns")} number={data['returns']} />
+        </Grid>
+      </Grid>
+    </Grid>
+  </Grid>;
 }
